fd5c5cbfea5b35f69360c3cd9f5b366a8aadae52
kinton <kintonk@yandex.ru>
2022-11-21 01:13:09 +0300

    Add decorator for get call and message from incoming aiogram action

--- bot/lib/telegram/aiogram/message_processor.py
+++ bot/lib/telegram/aiogram/message_processor.py
@@ -1,1 +1,12 @@
+from aiogram import types
 
+
+def call_and_message_accessed_processor(func):
+    async def _wrap(call_or_message: types.Message | types.CallbackQuery, *args, **kwargs):
+        # Call, Message, ...
+        if hasattr(call_or_message, 'data'):
+            await func(call_or_message, call_or_message.message, *args, **kwargs)
+        else:
+            await func(None, call_or_message, *args, **kwargs)
+
+    return _wrap
