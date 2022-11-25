c82c8293c78d285e6f723153a2b30d01db9e49ae
kinton <kintonk@yandex.ru>
2022-11-25 13:17:00 +0300

    Add empty photo link

--- bot/pkg/config/config.py
+++ bot/pkg/config/config.py
@@ -6,3 +6,5 @@ environment = {
     "TELEGRAM_BOT_TOKEN": os.environ['TELEGRAM_BOT_TOKEN'],
     "REDIS_PASSWORD": os.environ['REDIS_PASSWORD']
 }
+
+empty_photo_link = "https://cdn3.iconfinder.com/data/icons/online-states/150/Photos-512.png"
