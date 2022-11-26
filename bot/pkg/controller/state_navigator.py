d7affe168cdd8554d532e83696cc16651766f5e0
kinton <kintonk@yandex.ru>
2022-11-26 16:34:21 +0300

    State navigator improves

--- bot/pkg/controller/state_navigator.py
+++ bot/pkg/controller/state_navigator.py
@@ -1,6 +1,7 @@
 from pkg.service import user_storage
 
 from pkg.config.routes import RouteMap
+from pkg.system.logger import logger
 
 
 async def go_back(call):
@@ -9,12 +10,12 @@ async def go_back(call):
         # lastText = storage.get_user_last_text(call.message.chat.id)
         prev, curr = user_storage.prev_curr_states(call.message.chat.id)
 
-        method = RouteMap.get_method(prev)
+        method = RouteMap.get_route(prev, "method")
         if method is None:
             # TODO: show error
             return
 
-        await method(call, change_user_state=False)
+        await method(call, call.message, change_user_state=False)
 
         user_storage.go_back(call.message.chat.id)
 
