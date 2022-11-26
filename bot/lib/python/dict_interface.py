35b04a239465cdaade9b8455f89aa7625af95a79
kinton <kintonk@yandex.ru>
2022-11-26 16:31:16 +0300

    Add validate struct interface function

---
+++ bot/lib/python/dict_interface.py
@@ -0,0 +1,15 @@
+def validate_structure(struct, conf):
+    if isinstance(struct, dict) and isinstance(conf, dict):
+        # struct is a dict of types or other dicts
+        return all(k in conf and validate_structure(struct[k], conf[k]) for k in struct)
+    if isinstance(struct, list) and isinstance(conf, list):
+        # struct is list in the form [type or dict]
+        return all(validate_structure(struct[0], c) for c in conf)
+    elif isinstance(conf, type):
+        # struct is the type of conf
+        return isinstance(struct, conf)
+    elif isinstance(conf, tuple):
+        return struct in conf
+    else:
+        # struct is neither a dict, nor list, not type
+        return False
