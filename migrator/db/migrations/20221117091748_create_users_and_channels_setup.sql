abd766cc57f3bbc7ae238c5db0f87335cf11410a
kinton <kintonk@yandex.ru>
2022-11-29 15:41:00 +0300

    Update initial db structure

--- migrator/db/migrations/20221117091748_create_users_and_channels_setup.sql
+++ migrator/db/migrations/20221117091748_create_users_and_channels_setup.sql
@@ -7,10 +7,11 @@ create table users (
     created_at timestamp not null,
     updated_at timestamp not null
 );
-create index "service_id_index" on "users" ("service_id");
+create index "service_id_index_on_users" on "users" ("service_id");
 
-create table moderated_groups (
+create table moderated_chats (
     id bigserial primary key not null,
+    service_id varchar(255) not null,
     active boolean not null default true,
     disabled boolean not null default false,
     allow_administrators boolean not null default true,
@@ -18,20 +19,23 @@ create table moderated_groups (
     created_at timestamp not null,
     updated_at timestamp not null
 );
+create index "service_id_index_on_moderated_chats" on "moderated_chats" ("service_id");
 
-create table user_moderated_groups_connections (
+create table user_moderated_chat_connections (
+    id bigserial primary key not null,
     user_id bigint not null,
-    moderated_group_id bigint not null,
+    moderated_chat_id bigint not null,
     created_at timestamp not null,
     updated_at timestamp not null,
-    primary key (user_id, moderated_group_id),
+--    primary key (user_id, moderated_chat_id),
     constraint fk_user foreign key(user_id) references users(id),
-    constraint fk_moderated_group foreign key(moderated_group_id) references moderated_groups(id)
+    constraint fk_moderated_chat foreign key(moderated_chat_id) references moderated_chats(id),
+    unique (user_id, moderated_chat_id)
 );
 
 create table allowed_users (
     id bigserial primary key not null,
-    moderated_group_id bigint not null,
+    moderated_chat_id bigint not null,
     nickname varchar(255),
     service_id varchar(255),
     active boolean not null,
@@ -43,23 +47,23 @@ create table allowed_users (
     ban_expiration_date timestamp,
     created_at timestamp not null,
     updated_at timestamp not null,
-    constraint fk_moderated_group foreign key(moderated_group_id) references moderated_groups(id)
+    constraint fk_moderated_chat foreign key(moderated_chat_id) references moderated_chats(id)
 );
 
-create table moderated_group_statistics (
+create table moderated_chat_statistics (
     id bigserial primary key not null,
-    moderated_group_id bigint not null,
+    moderated_chat_id bigint not null,
     deleted_messages_count bigint default 0,
     created_at timestamp not null,
     updated_at timestamp not null,
-    constraint fk_moderated_group foreign key(moderated_group_id) references moderated_groups(id)
+    constraint fk_moderated_chat foreign key(moderated_chat_id) references moderated_chats(id)
 );
 
 -- migrate:down
 
-drop table moderated_group_statistics;
+drop table moderated_chat_statistics;
 drop table allowed_users;
-drop table user_moderated_groups_connections;
-drop table moderated_groups;
+drop table user_moderated_chat_connections;
+drop table moderated_chats;
 drop table users;
 
