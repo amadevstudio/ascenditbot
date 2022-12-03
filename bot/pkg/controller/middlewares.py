36499e12d712d19b7de5867de10a0f51d811ef4c
kinton <kintonk@yandex.ru>
2022-12-03 20:54:50 +0300

    Add comments for middleware

--- bot/pkg/controller/middlewares.py
+++ bot/pkg/controller/middlewares.py
@@ -8,12 +8,13 @@ class NoWhereInputProcessorMiddleware(BaseMiddleware):
     def __init__(self):
         super(NoWhereInputProcessorMiddleware, self).__init__()
 
-
     # async def on_process_update(self, update: types.Update (call or message), data: dict):
     async def on_pre_process_message(self, message: types.Message, data: dict):
-
         # handler = current_handler.get()
         # dispatcher = Dispatcher.get_current()
+
+        # data["some_param"] = "Данные"
+        # Using: @dispatcher; async def handler(message, middleware_data)
 
         state_navigator.nowhere_input_processor(message)
 
