811067adf9ba15789e0050f62489b9ceaa46aa83
kinton <kintonk@yandex.ru>
2022-12-15 18:36:00 +0300

    Use unite data approach for getting user state

--- bot/framework/controller/state_data.py
+++ bot/framework/controller/state_data.py
@@ -9,9 +9,11 @@ def get_current_state_data(call: types.CallbackQuery, message: types.Message, ro
     try:
         state_data = UserStorage.get_user_state_data(message.chat.id, route)
     except json.decoder.JSONDecodeError:
-        state_data = None
+        state_data = {}
 
-    if state_data is None:
-        state_data = json.loads(call.data)
+    try:
+        call_data = json.loads(call.data)
+    except:
+        call_data = {}
 
-    return state_data
+    return state_data | call_data
