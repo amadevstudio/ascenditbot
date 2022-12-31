b82ea5abe54f82728c182feb90caa51636e728f3
kinton <kintonk@yandex.ru>
2023-01-01 01:38:00 +0300

    Refactor UserRepository and add get_id_by_service_id func

--- bot/pkg/repository/user_repository.py
+++ bot/pkg/repository/user_repository.py
@@ -1,11 +1,10 @@
 from pkg.repository.database_connection import Database
+from project.types import UserInterface
 
 db = Database()
 
 
-def register_or_update_by_service_id(user_data) -> id:
-    user_data['service_id'] = str(user_data['service_id'])
-
+def register_or_update_by_service_id(user_data: UserInterface) -> int:
     user = db.fetchone("""
         SELECT id FROM users WHERE service_id = %s
     """, (user_data["service_id"],))
@@ -16,5 +15,16 @@ def register_or_update_by_service_id(user_data) -> id:
 
     user_data['id'] = user['id']
     user = db.update_model('users', user_data)
+
+    return user['id']
+
+
+def get_id_by_service_id(service_id: str) -> int | None:
+    user = db.fetchone("""
+        SELECT id FROM users WHERE service_id = %s
+    """, (service_id,))
+
+    if user is None:
+        return None
 
     return user['id']
