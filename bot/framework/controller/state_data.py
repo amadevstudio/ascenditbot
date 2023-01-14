e9ddb5420648d9a5437bc0d07df9744eb4b86c27
kinton <kintonk@yandex.ru>
2023-01-15 01:10:02 +0300

    Add decode call data function for call.data

--- bot/framework/controller/state_data.py
+++ bot/framework/controller/state_data.py
@@ -1,11 +1,23 @@
 import json
+from typing import Any
 
 from aiogram import types
 
 from pkg.service.user_storage import UserStorage
+from pkg.system.logger import logger
 
 
-def get_current_state_data(call: types.CallbackQuery, message: types.Message, route: str):
+def decode_call_data(call: types.CallbackQuery) -> dict[str, Any]:
+    try:
+        return json.loads(call.data)
+    except Exception as e:
+        logger.error(e)
+        pass
+
+    return {}
+
+
+def get_current_state_data(call: types.CallbackQuery, message: types.Message, route: str) -> dict[str, Any]:
     try:
         state_data = UserStorage.get_user_state_data(message.chat.id, route)
     except json.decoder.JSONDecodeError:
@@ -13,7 +25,7 @@ def get_current_state_data(call: types.CallbackQuery, message: types.Message, ro
 
     try:
         call_data = json.loads(call.data)
-    except:
+    except Exception:
         call_data = {}
 
     return state_data | call_data
