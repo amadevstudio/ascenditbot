b7a14d52744abc4285eaf36db984e17f074bc66d
kinton <kintonk@yandex.ru>
2023-01-19 01:09:00 +0300

    Subscription handler schema structure

--- bot/pkg/background/async_tasks/subscription_handler.py
+++ bot/pkg/background/async_tasks/subscription_handler.py
@@ -11,7 +11,19 @@ async def subscription_handler():
         now = datetime.datetime.now()
         next_hour = (now + delta).replace(microsecond=0, second=0, minute=0)
         wait_seconds = (next_hour - now).seconds
-        # await asyncio.sleep(wait_seconds)
+        await asyncio.sleep(wait_seconds)
         await asyncio.sleep(1)  # Ensure unique per interval
 
-        Tariff.process_all_subscription_validity()
+        for process_subscription_data in Tariff.process_all_subscription_validity():
+            user = process_subscription_data['user']
+            if process_subscription_data['action'] == 'prolonged':
+                message = "prolonged!"
+
+                if process_subscription_data['prolongable'] is False:
+                    message += "can't prolong in future!"
+
+            elif process_subscription_data['action'] == 'disabled':
+                message = "tariff disabled! fill and re-subscribe!"
+
+            # Send message to user
+
