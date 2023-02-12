05e588d7a1e7090599574aa3ab641e60d252499b
kinton <kintonk@yandex.ru>
2023-02-12 14:54:01 +0300

    Notify admin about bot update

---
+++ bot/pkg/service/message_sender.py
@@ -0,0 +1,14 @@
+from framework.controller.message_tools import chat_id_sender
+from pkg.config.config import environment
+from pkg.controller import bot
+
+
+class MessageSender:
+    @staticmethod
+    async def notify_admins(text: str):
+        telegram_admin_group_id = environment.get('TELEGRAM_ADMIN_GROUP_ID', None)
+        if telegram_admin_group_id is not None:
+            await chat_id_sender(bot.bot, int(telegram_admin_group_id), message_structures=[{
+                'type': 'text',
+                'text': text
+            }])
