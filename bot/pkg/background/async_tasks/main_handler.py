37c69d8c54ab89dcd2abef993ad0a517a5835e6d
kinton <kintonk@yandex.ru>
2023-02-12 17:01:31 +0300

    Notify about started only in production mode

--- bot/pkg/background/async_tasks/main_handler.py
+++ bot/pkg/background/async_tasks/main_handler.py
@@ -2,13 +2,15 @@ import asyncio
 
 from aiogram import Dispatcher
 
+from pkg.config.config import environment
 from pkg.service.message_sender import MessageSender
 from pkg.background.async_tasks.subscription_handler import subscription_handler
 from pkg.system.logger import logger
 
 
 async def on_bot_startup(dispatcher: Dispatcher) -> None:
-    await MessageSender.notify_admins('The bot is started')
+    if environment['ENVIRONMENT'] != 'development':
+        await MessageSender.notify_admins('The bot is started')
     logger.log("The bot is started")
 
     asyncio.create_task(subscription_handler(dispatcher))
