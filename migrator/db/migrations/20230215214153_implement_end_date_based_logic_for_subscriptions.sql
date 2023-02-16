f3bab914d8ba91364eb3f46ffed745b39b876ac1
kinton <kintonk@yandex.ru>
2023-02-17 00:25:57 +0300

    Improve user_tariff_connections: to end_date migration

--- migrator/db/migrations/20230215214153_implement_end_date_based_logic_for_subscriptions.sql
+++ migrator/db/migrations/20230215214153_implement_end_date_based_logic_for_subscriptions.sql
@@ -3,7 +3,11 @@
 alter table user_tariff_connections
 rename column start_date to end_date;
 
+update user_tariff_connections set end_date = end_date + interval '30 day';
+
 -- migrate:down
 
 alter table user_tariff_connections
 rename column end_date to start_date;
+
+update user_tariff_connections set start_date = start_date - interval '30 day';
