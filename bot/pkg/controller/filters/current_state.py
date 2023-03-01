a9ce3c477258e51108a5ce652ba69790a3ec22ab
kinton <kintonk@yandex.ru>
2023-03-01 18:16:00 +0300

    Add custom filters to maintain routes

---
+++ bot/pkg/controller/filters/current_state.py
@@ -0,0 +1,25 @@
+from aiogram.filters import BaseFilter
+from aiogram.types import Message, CallbackQuery
+
+from framework.controller.router_tools import message_route_validator, user_state, get_type
+
+from pkg.config.routes import AvailableRoutes, RouteMap
+
+
+class CurrentStateMessageFilter(BaseFilter):
+    def __init__(self, routes: list[AvailableRoutes]):
+        self.routes = routes
+
+    async def __call__(self, message: Message) -> bool:
+        return message_route_validator(self.routes, message)
+
+
+class CurrentStateActionFilter(BaseFilter):
+    def __init__(self, route: AvailableRoutes, action: str):
+        self.route = route
+        self.action = action
+
+    async def __call__(self, call: CallbackQuery) -> bool:
+        return (
+            user_state(call) == RouteMap.state(self.route)
+            and get_type(call) == RouteMap.action_type(self.route, self.action))
