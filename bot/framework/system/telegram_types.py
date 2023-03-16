7216e7066d8282e92855a5d13a499f51b6f6967e
kinton <kintonk@yandex.ru>
2023-03-16 21:33:00 +0300

    Introduce telegram_types framework module

---
+++ bot/framework/system/telegram_types.py
@@ -0,0 +1,7 @@
+from aiogram import types as types
+
+
+CallbackQuery = types.CallbackQuery
+Message = types.Message
+InlineKeyboardMarkup = types.InlineKeyboardMarkup
+InlineKeyboardButton = types.InlineKeyboardButton
