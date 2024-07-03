67188c74dddce594503beee26e1acd2667c8760f
kinton <kintonk@yandex.ru>
2024-07-03 22:57:55 +0300

    Add privacy message

--- bot/pkg/config/routes_dict.py
+++ bot/pkg/config/routes_dict.py
@@ -2,7 +2,7 @@ from typing import Literal
 
 AvailableCommands = Literal['start', 'menu', 'add_chat', 'my_chats', 'subscription']
 AvailableRoutes = Literal[
-    'start', 'menu', 'add_chat', 'my_chats', 'chat',
+    'start', 'privacy', 'help', 'menu', 'add_chat', 'my_chats', 'chat',
     'add_to_chat_whitelist', 'chat_whitelist', 'allowed_user',
     'subscription', 'tariffs', 'fund', 'fund_amount',
     'settings', 'settings_email',
